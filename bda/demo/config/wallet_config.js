c5a66dbc52c7e7f6df27924f336523ac1291e0a9
Matus Remen <xremen01@stud.fit.vutbr.cz>
2024-04-07 17:59:25 +0200

    bda: upload demo template

---
+++ bda/demo/config/wallet_config.js
@@ -0,0 +1,8 @@
+// You can adjust these two parameters of tests to your n-of-m multisig wallet contract
+
+var MultiSigWalletConf = Object.freeze({
+    NUMBER_OF_OWNERS: 2, // m
+    REQUIRED_SIGS: 2, // n
+})
+
+module.exports = MultiSigWalletConf;
