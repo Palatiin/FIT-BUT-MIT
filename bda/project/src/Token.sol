7b8286d14f2d742dd9198633fd506aca6502e49d
Matus Remen <xremen01@stud.fit.vutbr.cz>
2025-04-11 15:37:44 +0200

    bda: implement mint function with t1.1, t1.2

--- bda/project/src/Token.sol
+++ bda/project/src/Token.sol
@@ -4,13 +4,16 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
-    uint256 public maxSupply;
-    uint256 public maxDailyMint;
+    uint256 public immutable MAX_SUPPLY;
+    uint256 public immutable MAX_DAILY_MINT;
+
+    uint256 public dailyMinted;
+    uint256 public mintDailyLimitResetTimestamp;
 
     mapping(address => bool) private isMintingAdmin;
 
     /**
-    * T1.1: extend ERC20 constructor to initialize maxSupply
+    * T1.1: extend ERC20 constructor to initialize MAX_SUPPLY 
     * T1.2: mintingAdmin role, maxDailyMint
     *
     * @param _maxSupply: maximum supply of the token
@@ -18,17 +21,52 @@ contract Token is ERC20 {
     * @param _maxDailyMint: maximum amount of tokens that can be minted in a day
     */
     constructor(uint256 _maxSupply, address[] memory _mintingAdmins, uint256 _maxDailyMint) ERC20("Token", "TKN") {
-        maxSupply = _maxSupply;
+        MAX_SUPPLY = _maxSupply;
         for (uint i = 0; i < _mintingAdmins.length; i++) {
             checkNotNull(_mintingAdmins[i]);
             isMintingAdmin[_mintingAdmins[i]] = true;
         }
-        maxDailyMint = _maxDailyMint;
+        MAX_DAILY_MINT = _maxDailyMint;
     }
+
+    /**
+    * T1.1, T1.2
+    *
+    * @param to: address of the recipient
+    * @param amount: amount of tokens to mint
+    */
+    function mint(address to, uint256 amount) public onlyMintingAdmin checkDailyMintLimit(amount) {
+        require(totalSupply() + amount <= MAX_SUPPLY, "Max supply exceeded");
+
+        _mint(to, amount);
+        dailyMinted += amount;
+    }
+
+    // ===== View Functions =====
+    function getCurrentTimestamp() internal view returns (uint256) {
+        return block.timestamp;
+    }
+
+    // ===== Pure Functions =====
 
     function checkNotNull(address _address) internal pure {
         if (_address == address(0x0)) {
             revert("Address cannot be zero.");
         }
+    }
+
+    // ===== Modifiers =====
+    modifier onlyMintingAdmin() {
+        require(isMintingAdmin[_msgSender()], "Not a minting admin");
+        _;
+    }
+
+    modifier checkDailyMintLimit(uint256 amount) {
+        if (getCurrentTimestamp() >= mintDailyLimitResetTimestamp) {
+            dailyMinted = 0;
+            mintDailyLimitResetTimestamp = getCurrentTimestamp() + 1 days;
+        }
+        require(dailyMinted + amount <= MAX_DAILY_MINT, "Max daily mint exceeded");
+        _;
     }
 }
--- bda/project/test/Token.t.sol
+++ bda/project/test/Token.t.sol
@@ -6,4 +6,70 @@ import {Token} from "../src/Token.sol";
 
 contract TokenTest is Test {
     Token public token;
+    address[] public addresses;
+
+    /**
+     * 5 addresses, first 2 are minting admins.
+     */
+    function initToken(uint256 _maxSupply, uint256 _maxDailyMint) public {
+        delete addresses;
+
+        for (uint i = 1; i < 5; i++) {
+            addresses.push(vm.addr(i));
+        }
+
+        address[] memory mintingAdmins = new address[](2);
+        mintingAdmins[0] = addresses[0];
+        mintingAdmins[1] = addresses[1];
+
+        token = new Token(_maxSupply, mintingAdmins, _maxDailyMint);
+    }
+
+    function test_mint_admin() public {
+        initToken(1000, 100);
+        assertEq(token.totalSupply(), 0, "Total supply should be 0");
+
+        // Test minting 10 tokens
+        vm.prank(addresses[0]);
+        token.mint(addresses[2], 10);
+        assertEq(token.totalSupply(), 10, "Total supply should be 10");
+        assertEq(token.balanceOf(addresses[2]), 10, "Balance of 2 should be 10");
+
+        // Test non-admin cannot mint
+        vm.expectRevert("Not a minting admin");
+        vm.prank(addresses[2]);
+        token.mint(addresses[3], 10);
+    }
+
+    function test_mint_over_total_supply() public {
+        initToken(100, 200);
+        assertEq(token.totalSupply(), 0, "Total supply should be 0");
+
+        vm.expectRevert("Max supply exceeded");
+        vm.prank(addresses[0]);
+        token.mint(addresses[2], 110);
+    }
+
+    function test_mint_daily_limit() public {
+        initToken(1000, 100);
+        assertEq(token.totalSupply(), 0, "Total supply should be 0");
+
+        // Test minting 100 tokens
+        vm.prank(addresses[0]);
+        token.mint(addresses[2], 100);
+        assertEq(token.totalSupply(), 100, "Total supply should be 100");
+        assertEq(token.balanceOf(addresses[2]), 100, "Balance of 2 should be 100");
+
+        // Test daily mint limit
+        vm.expectRevert("Max daily mint exceeded");
+        vm.prank(addresses[0]);
+        token.mint(addresses[2], 100);
+
+        // Test daily mint limit reset
+        vm.warp(block.timestamp + 1 days);
+        vm.prank(addresses[0]);
+        token.mint(addresses[2], 100);
+        assertEq(token.totalSupply(), 200, "Total supply should be 200");
+        assertEq(token.balanceOf(addresses[2]), 200, "Balance of 2 should be 200");
+    }
 }
